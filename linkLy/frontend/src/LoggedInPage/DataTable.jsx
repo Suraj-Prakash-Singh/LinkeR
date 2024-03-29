06c9664ec30418f43b5d09e31fc93e27a476802b
TesnSai <shivamchaudhary8476@gmail.com>
2024-03-29 23:56:41 +0530

    refactoring and initailised shadcn-ui

---
+++ linkLy/frontend/src/LoggedInPage/DataTable.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const DataTable = () => {
+
+    return (
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="table-container w-10/12 h-max">
+                
+            </div>
+        </div>
+    )
+}
+
+export default DataTable;
--- linkLy/frontend/src/LoggedInPage/LoggedIn.jsx
+++ linkLy/frontend/src/LoggedInPage/LoggedIn.jsx
@@ -1,6 +1,8 @@
 import InputBox from "../InputBox";
+import DataTable from "./DataTable";
 import chevronDown from "../assets/chevron-down.svg"
 import bell from "../assets/bell.svg";
+
 const LoggedIn = () => {
 
     return (
@@ -12,7 +14,7 @@ const LoggedIn = () => {
                     <div className="w-7/12">
                         <InputBox width={"w-full"} height={"h-12"}></InputBox>
                     </div>
-                    <div className="w-max flex items-center gap-3">
+                    <div className="w-max flex items-center gap-6">
                         <button className="flex items-center py-[4px] px-4 rounded-[28px] border border-[#C9CED6] bg-[#353C4A]">
                             <div className="flex items-center justify-between gap-3">
                                 <div className="flex flex-col items-start">
@@ -28,7 +30,9 @@ const LoggedIn = () => {
                     </div>
                 </div>
             </div>
-            <div className="w-full h-3/4 bg-[#151A24]"></div>
+            <div className="w-full h-3/4 bg-[#151A24] flex items-center justify-center">
+                <DataTable></DataTable>
+            </div>
         </div>
     ) 
 }
